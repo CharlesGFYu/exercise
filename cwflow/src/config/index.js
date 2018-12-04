9e135484c81fb82fe9186d4ada5c50e69716aea0
v_guifuyu(余贵福) <v_guifuyu@tencent.com>
2018-12-04 19:01:21 +0800

    commit files

---
+++ cwflow/src/config/index.js
@@ -0,0 +1,3 @@
+const NPM = 'npm'
+
+module.exports = {NPM}
--- cwflow/src/tasks/init.js
+++ cwflow/src/tasks/init.js
@@ -2,7 +2,62 @@ const fs = require('fs')
 const chalk = require('chalk')
 const runSeries = require('run-series')
 const ora = require('ora')
-const spawn = require('cross-spawn')
+const crossSpawn = require('cross-spawn')
 const copyTemplateDir = require('copy-template-dir')
 const {NPM} = require($src_path('./config/'))
 
+const installDependencies = (targetDir, cb) => {
+  const spinner = ora(chalk.green(`Install dependencies...`)).start()
+  const cmd = crossSpawn(NPM, ['install', '--slient', '--no-progress', '--no-package-lock', '--production'], {
+    cwd: targetDir,
+    stdio: ['ignore', 'pipe', 'inherit']
+  })
+  cmd.on('close', (code) => {
+    if(code !== 0) {
+      spinner.fail()
+      console.log(chalk.red(`${NPM} install failed`))
+      return cb(new Error(`${NPM} install failed`))
+    }
+    spinner.succeed()
+    cb()
+  })
+}
+
+const initProject = (type, dirname) => {
+  if(!dirname) {
+    console.log(chalk.red('Please input dirname!'))
+    process.exit()
+  }
+
+  console.log(chalk.green('Initializing...'))
+  
+  const targetDir = $path.resolve(dirname)
+  if(fs.existsSync(targetDir)) {
+    console.log(chalk.red(`${targetDir} already exists`))
+    process.exit()
+  }
+
+  const templateDir = $root_path(`./templates/${type}`)
+  runSeries(
+    [
+      (cb) => {
+        copyTemplateDir(templateDir, targetDir, {
+          name: dirname,
+          version: $version
+        }, (err, createdFiles) => {
+          if(err) throw err          
+          createdFiles.forEach((filePath) => console.log(`   ${chalk.cyan('create')} ${filePath}`))
+          cb()
+        })
+      },
+      (cb) => {
+        installDependencies(targetDir, cb)
+      }
+    ],
+    (err, results) => {
+      console.log(chalk.green('The project initialization is complete!'))
+    }
+  )
+}
+
+module.exports = initProject
